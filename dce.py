ef5a904984aaf9033f4546a852b5e115735a8abc
phanthanhphu <phub1706742@gmail.com>
2022-08-14 10:47:54 +0700

    Branch C

--- dce.py
+++ dce.py
@@ -1,4 +1,38 @@
-a  = 0101010
-print("helo abc")
-print("hello erveryone ")
-print("pull")
+import React, {Component} from 'react';
+import './css/bootstrap.min.css';
+class Header extends Component {
+    render() {
+        return (
+            <header className="masthead bg-primary text-white text-center" color='background-color: #0d0d0e!important;'>
+                <div className="container d-flex align-items-center flex-column">
+                    <img className="masthead-avatar mb-5" src="assets/img/avataaars.svg" alt="" />
+                    {/* Masthead Heading*/}
+                  
+                    {/* Icon Divider*/}
+                    <div className="divider-custom divider-light">
+                        <div className="divider-custom-line" />
+                        <div className="divider-custom-icon"><i className="fas fa-star" /></div>
+                        <div className="divider-custom-line" />
+                    </div>
+                    {/* Masthead Subheading*/}
+                    <p className="masthead-subheading font-weight-light mb-0">content</p>
+                </div>
+ <div className="container d-flex align-items-center flex-column">
+                    <img className="masthead-avatar mb-5" src="assets/img/avataaars.svg" >
+                    {/* Masthead Heading*/}
+
+                    {/* Icon Divider*/}
+                    <div className="divider-custom divider-light">
+                        <div className="divider-custom-line" />
+                        <div className="divider-custom-icon"><i className="fas fa-star" />
+                        <div className="divider-custom-line" />
+                    </div>
+                    {/* Masthead Subheading*/}
+                    <p className="masthead-subheading font-weight-light mb-0">content</p>
+                </div>
+            </header>
+        );
+    }
+}
+
+export default Header;
